4a4482b28db207f3cb07ec68559b63d2111aea0f
Usuario de Salon <aulas@academico.icesi.edu.co>
2019-05-14 18:14:08 -0500

    Created the University class

--- src/model/AcademicUnit.java
+++ src/model/AcademicUnit.java
@@ -1,5 +1,52 @@
 package model;
 
-public class AcademicUnit {
+public class AcademicUnit implements Comparable<AcademicUnit>{
+	private String name;
+	private int code;
+	private String director;
+	private AcademicUnit right;
+	private AcademicUnit left;
+	
+	
+	public AcademicUnit(String n, int c, String d) {
+		name = n;
+		code = c;
+		director = d;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getCode() {
+		return code;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public AcademicUnit getRight() {
+		return right;
+	}
+	public AcademicUnit getLeft() {
+		return left;
+	}
+	public void setRight(AcademicUnit r) {
+		right = r;
+	}
+	public void setLeft(AcademicUnit l) {
+		left = l;
+	}
+
+	@Override
+	public int compareTo(AcademicUnit other) {
+		int comparation;
+		if(code > other.code) {
+			comparation = 1;
+		}else if(code < other.code) {
+			comparation = -1;
+		}else {
+			comparation = 0;
+		}
+		return comparation;
+	}
+	
 
 }
--- src/model/University.java
+++ src/model/University.java
@@ -1,5 +1,40 @@
 package model;
 
 public class University {
+	private String name;
+	private String address;
+	private AcademicUnit root;
+	
+	public University(String n, String a) {
+		this.name = n;
+		this.address = a;
+	}
+	
+	public void addAcademicUnit(String n, int c, String d) {
+		AcademicUnit academicUnit = new AcademicUnit(n,c,d);
+		if(root == null) {
+			root = academicUnit;
+		}else {
+			AcademicUnit current = root;
+			boolean added = false;
+			while(!added) {
+				if(academicUnit.compareTo(current) > 0) {
+					if(current.getRight() == null) {
+						current.setRight(academicUnit);
+						added = true;
+					}else {
+						current = current.getRight();
+					}
+				}else {
+					if(current.getLeft() == null) {
+						current.setLeft(academicUnit);
+						added = true;
+					}else {
+						current = current.getLeft();
+					}
+				}
+			}
+		}
+	}
 
 }
